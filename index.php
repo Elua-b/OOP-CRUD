a88eebd04a0f070d618c96dcf3dfcb1e38632e1c
Elua-b <bugingoeloi@gmail.com>
2022-06-13 11:24:06 +0200

    delete and update

--- index.php
+++ index.php
@@ -7,7 +7,17 @@ if(isset($_POST['submit']) ){
     // print_r($obj); 
     $obj->insertRecord($_POST);
 }
-
+if(isset($_POST['update']) ){
+  // echo("hello");
+  // print_r($obj); 
+  $obj->updateRecord($_POST);
+}
+if(isset($_GET['deleteid']) ){
+  // echo("hello");
+  // print_r($obj); 
+  $delid=$_GET['deleteid'];
+  $obj->deleteRecord($delid);
+}
 
 ?>
 
@@ -23,18 +33,46 @@ if(isset($_POST['submit']) ){
 <body>
     <h1>OOP CRUD</h1>
     <div class="container">
-    <form action="index.php" method="post">
+        <?php
+        if(isset($_GET['msg']) AND $_GET['msg']=='ins'){
+            echo '<div>Record Inserted successfully...!</div>';
+        }
+        if(isset($_GET['msg']) AND $_GET['msg']=='ups'){
+          echo '<div>Record updated successfully...!</div>';
+      }
+      if(isset($_GET['msg']) AND $_GET['msg']=='del'){
+        echo '<div>Record deleted successfully...!</div>';
+    }
+        ?>
+        <?php 
+        if(isset($_GET['editid'])){
+  $editid=$_GET['editid'];
+  $myrecord=$obj->displayRecordById($editid);
+  ?>
+   <form action="index.php" method="post">
     <label for="fname">Name</label>
-    <input type="text" id="name" name="name" placeholder="Your name..">
+    <input type="text" id="name" value="<?php echo $myrecord['name'];?>" name="name" placeholder="Your name.."/>
 
     <label for="lname">Email</label>
-    <input type="email" id="email" name="email" placeholder="Your Email..">
+    <input type="email" id="email" name="email"  value="<?php echo $myrecord['email'];?>"placeholder="Your Email.."/>
 
     
-  
-    <input type="submit" name="submit" value="Submit">
+  <input type="hidden" name="hid" value="<?php echo $myrecord['id'];?>">
+    <input type="submit" name="update" value="update"/>
   </form>
-  <h1>A Fancy Table</h1>
+  <?php
+} else{
+?>
+    <form action="index.php" method="post">
+    <label for="fname">Name</label>
+    <input type="text" id="name"  name="name" placeholder="Your name.."/>
+
+    <label for="lname">Email</label>
+    <input type="email" id="email" name="email" placeholder="Your Email.."/>
+    <input type="submit" name="submit" value="Submit"/>
+  </form>
+  <?php }?>
+  <h1>Registration Table</h1>
 
 <table id="customers">
   <tr>
@@ -43,6 +81,31 @@ if(isset($_POST['submit']) ){
     <th>Email</th>
     <th>Action</th>
   </tr>
+  <?php
+  $data=$obj->displayRecord();
+  $sno=1;
+  foreach($data as $value){
+    ?>
+
+    <tr>
+      <td>
+        <?php echo $sno++;?>
+      </td>
+      <td>
+        <?php echo $value['name'];?>
+      </td>
+      <td>
+        <?php echo $value['email'];?>
+      </td>
+      <td id="action">
+        <a href="index.php?editid=<?php echo $value['id'];?>">Edit</a>
+        <a  id="del" href="index.php?deleteid=<?php echo $value['id'];?>">Delete</a>
+
+      </td>
+    </tr>
+    <?php
+  }
+  ?>
   
   
 </table>
--- model.php
+++ model.php
@@ -17,17 +17,58 @@ class Model{
     } 
     public function insertRecord($post){
         // echo 'insert function call';
+        
         $name=$post['name'];
         $email=$post['email'];
         $sql="INSERT INTO users(name,email)VALUE('$name','$email')";
         $result=$this->conn->query($sql);
         if($result){
-            header('location:inde.php?msg=ins');
+            header('location:index.php?msg=ins');
         }else{
             echo "Error ".$sql. "<br/>".$this->conn->error;
         }
     }
+    public function updateRecord($post){
+        // echo 'insert function call';
+        
+        $name=$post['name'];
+        $email=$post['email'];
+        $editid=$post['hid'];
+        $sql="UPDATE users SET name='$name', email='$email' WHERE id=$editid";
+        $result=$this->conn->query($sql);
+        if($result){
+            header('location:index.php?msg=ups');
+        }else{
+            echo "Error ".$sql. "<br/>".$this->conn->error;
+        }
+    }
+    public function displayRecord(){
+        $sql="SELECT * FROM users";
+        $result=$this->conn->query($sql);
+        if($result->num_rows>0){
+            while($row=$result->fetch_assoc()){
+                $data[]=$row;
+            }
+            return $data;
+        }
+    }
+    public function displayRecordById($editid){
+        $sql="SELECT * FROM users WHERE id='$editid'";
+        $result= mysqli_query($this->conn,$sql);
+        if(mysqli_num_rows($result)==1){
+            $row=$result->fetch_assoc();
+            
+            return $row;
+        }
+    }
+    public function deleteRecord($delid){
+        $sql="DELETE FROM  users Where id='$delid'";
+        $result=$this->conn->query($sql);
+        if($result){
+            header('location:index.php?msg=del');
+        }else{
+            echo "Error".$sql."<br/>".$this->conn->error;
+        }
+    }
 }
 $obj=new Model();
-
-?>
